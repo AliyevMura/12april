71778337dde4def1fc98e991900e7f4caaf6fcad
AliyevMura <7lvghl9@code.edu.az>
2023-04-12 02:13:20 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Encaptulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //properti usulu
+
+           // Person person = new Person();
+           // person.name = "Murad";
+           // Console.WriteLine(person.name);
+           // person.Surname = "Aliyev";
+           // Console.WriteLine(person.Surname);
+           // person.age = 19;
+           // Console.WriteLine(person.age);
+           //person.BirthDay = DateTime.Now;
+           // Console.WriteLine(person.BirthDay);
+
+
+            //Encaptulation 
+            //Person person = new Person();
+            //person.SetName("Murad");
+            //string name = person.GetName();
+
+            //Console.WriteLine(name);
+            //person.SetAge(19);
+            //int age = person.GetAge();
+        }
+    }
+}
---
+++ class/Person.cs
@@ -0,0 +1,111 @@
+﻿
+
+namespace Encaptulation
+{
+    internal class Person
+
+    {
+        //properti
+        //private string _name;
+        //public string Name
+        //{
+        //    get
+        //    {
+        //        return _name;
+        //    }
+        //    set
+        //    {
+        //        if(value.Length<2) {
+        //            Console.WriteLine("adiniz 2 herfden boyuk olmalidir");
+        //        }
+        //        _name = value;
+        //        return;
+        //    }
+        //}
+        //private string _surname;
+        //public string Surname
+        //{
+        //    get { return _surname; }
+        //    set
+        //    {
+        //        _surname = value;
+        //    }
+        //}
+
+
+        //private int _age;
+        //public int Age
+        //{
+        //    get
+        //    {
+        //        return _age;
+        //    }
+        //    set { _age = value;
+        //        return;
+        //    }
+        //}
+        //private DateTime _birthday;
+        //public DateTime Birthday
+        //{
+        //    get
+        //    {
+        //        return _birthday;
+        //    }
+        //    set
+        //    {
+        //        _birthday = value;
+        //        return;
+        //    }
+        //}
+        //public string name { get; set; }
+        //public string surname { get; set; }
+        //public int age { get; set; }
+        //public DateTime BirthDay { get; set; }
+
+
+
+
+        //Encaptulation 
+        //private string _name;
+        //public void SetName(string value)
+        //{
+        //    if (value.Length > 2)
+        //    {
+        //        _name = value;
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("adiniz 2 herfden balacadir");
+        //    }
+        //}
+        //public string GetName()
+        //{
+        //    return _name;
+        //}
+
+
+        //private int _age;
+        //public void SetAge(int value)
+        //{
+        //    if (value < 16)
+        //    {
+        //        Console.WriteLine("yasiniz catmir!");
+        //    }
+        //    else
+        //    {
+        //        _age = value;
+        //        return;
+        //    }
+        //}
+        //public int GetAge()
+        //{
+        //    return _age;
+        //}
+
+
+    }
+
+
+}
+
